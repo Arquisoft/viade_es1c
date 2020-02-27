e5a3b214184ad149e20e0549a2359e8d3eadeb8a
Miguel Ornia <UO250732@uniovi.es>
2020-02-27 12:52:00 +0100

    Adding some structure

---
+++ src/components/button.js
@@ -0,0 +1,21 @@
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        );
+    }
+}
