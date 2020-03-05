e9c7dc1c46dcdf51397284dbf01ee6bb49d99a84
Alberto <uo257180@uniovi.es>
2020-03-05 17:30:55 +0100

    Adding viade logo

--- src/components/NavBar/nav-bar.component.js
+++ src/components/NavBar/nav-bar.component.js
@@ -58,7 +58,7 @@ const NavBar = (props: Props) => {
       <section className="header-wrap">
         <div className="logo-block">
           <Link to="/welcome">
-            <img src="/img/inrupt.svg" alt="inrupt" />
+            <img src="/img/logoViaDe.svg" alt="inrupt" />
           </Link>
         </div>
 
