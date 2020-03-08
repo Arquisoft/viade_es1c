f8408790e4924b6bb291aec7e8a93875ae8d8f8f
Miguel Ornia <UO250732@uniovi.es>
2020-03-08 22:15:12 +0100

    Improving map

--- src/containers/VisualizeMap/vMapa.component.js
+++ src/containers/VisualizeMap/vMapa.component.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { render } from 'react-dom'
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 class VMapaComponent extends React.Component {
   constructor(){
@@ -15,7 +18,7 @@ class VMapaComponent extends React.Component {
   render() {
     const position = [this.state.lat, this.state.lng];
     return (
-      <Map center = {position} zoom = {this.state.zoom}>
+      <Map center = {position} zoom = {this.state.zoom} style={{width: '100%',height: '400px'}}>
         <TileLayer url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       </Map>
     );
