3ae09a987ac86131d7c0cfe8e00ce41ef9060402
Miguel Ornia <UO250732@uniovi.es>
2020-03-09 15:09:54 +0100

    Adding select for routes

--- src/containers/VisualizeMap/Map/vMap.component.js
+++ src/containers/VisualizeMap/Map/vMap.component.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Marker, Popup, TileLayer } from "react-leaflet";
-import { MapStyled, MapWrapper } from './vMap.style';
+import { MapStyled, MapWrapper, CenterWrapper, SelectStyled} from './vMap.style';
 import { useTranslation } from "react-i18next";
-import { Combobox } from 'react-widgets';
 
 /**
  * Component used to display routes on a map
@@ -14,33 +13,24 @@ export const  VMapComponent = props => {
     lng: -5.85166,
     zoom: 12
   }
-  //Sustituir por las rutas del pod
-  const data=['ruta1','ruta2','ruta3'];
-  
+  // Sustituir por las rutas del POD
+  const data=['Ruta1','Ruta2','Ruta3'];
   const { t } = useTranslation();
   const position = [state.lat, state.lng];
-  const {combo}= verCombo();
 
   return (
     <MapWrapper>
       <h1>{t('routes.title')}</h1>
-      <div>
-        {combo}
-      </div>
-      <MapStyled center = {position} zoom = {state.zoom} >
-        <TileLayer url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      </MapStyled>
+      <CenterWrapper>
+        <label>{t('routes.select')}</label>
+        <SelectStyled options={data}/>
+        <MapStyled center = {position} zoom = {state.zoom} >
+          <TileLayer url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        </MapStyled>
+      </CenterWrapper>
     </MapWrapper>
   );
 }
 
-function verCombo(){
-  return (
-    <Combobox 
-      data={['ruta1','ruta2']}
-      defaultValue={"ruta1"}
-    />
-  );
-}
 
 export default VMapComponent;
--- src/containers/VisualizeMap/Map/vMap.style.js
+++ src/containers/VisualizeMap/Map/vMap.style.js
@@ -1,17 +1,27 @@
 import styled from 'styled-components';
 import { Map } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
-
+import { Select } from '@util-components';
 
 export const MapWrapper = styled.section`
   width: 100%;
   background-image: url('/img/background-image.jpg');
   background-repeat: repeat;
   text-align:center;
+  position: relative;
+`;
+
+export const CenterWrapper = styled.div`
+  width: 100%;
+`;
+
+export const SelectStyled = styled(Select)`
+   width: 10%;
+   background: #fff;
 `;
 
 export const MapStyled = styled(Map)`
     width: 80%;
     height: 500px;
     display: inline-block;
-  `;
+`;
