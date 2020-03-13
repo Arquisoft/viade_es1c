10d4c0a75db9777f57f63fed74a2e081e067bcef
NourdineSatour <UO265379@uniovi.es>
2020-03-13 14:41:21 +0100

    Create a folde in the pod

--- src/services/addRoute.js
+++ src/services/addRoute.js
@@ -1,3 +1,30 @@
 import { fetchDocument } from 'tripledoc';
 
 const auth = require('solid-auth-client')
+
+export async function addRoute(routeToAdd) {
+    let session = await auth.currentSession();
+    if (!session) { 
+        window.location.href = "/login"; 
+    }
+    const dir = 'private/routes/' + routeToAdd.nombre + '.tll';
+    const webId = session.webId;
+    
+    await newDocument(webId, dir);
+
+}
+
+async function newDocument(webId, dir) {
+
+    const profileDocument = await fetchDocument(webId);
+    const profile = profileDocument.getSubject(webId);
+
+
+    const storage = profile.getRef(space.storage);
+
+
+    const routesListRef = storage + dir;
+
+    const routesList = createDocument(routesListRef);
+    await routesList.save();
+}
