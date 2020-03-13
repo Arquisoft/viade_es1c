f0a57c8ab640239f03c14d021856e03b964056f4
NourdineSatour <UO265379@uniovi.es>
2020-03-13 14:55:49 +0100

    Begin the upload with the solid-file-client library

--- src/containers/UploadComponent/upload.component.js
+++ src/containers/UploadComponent/upload.component.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { UploadWrapper, H1, MyInput } from './upload.style';
 import { useTranslation } from "react-i18next";
+import auth from "solid-auth-client";
+import FC from 'solid-file-client';
+
 
 
 export const UploadComponent = props => {
+
+  const fileReader = new FileReader();
   const { t } = useTranslation();
   return(
     <UploadWrapper>
       <H1>{t('upload.title')}</H1>
       <MyInput type="file"/>
+      
     </UploadWrapper>
   );
 }
