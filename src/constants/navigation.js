5b9e891e386b81567a049a4646043e7128a05aa1
Miguel Ornia <UO250732@uniovi.es>
2020-03-14 17:44:02 +0100

    Slight modifications

--- src/constants/navigation.js
+++ src/constants/navigation.js
@@ -41,14 +41,6 @@ export const ProfileOptions = [
     icon: 'cog'
   },
   {
-    label: 'navBar.formModelConvert',
-    onClick: 'formModelConvertRedirect'
-  },
-  {
-    label: 'navBar.formModelRender',
-    onClick: 'formModelRenderRedirect'
-  },
-  {
     label: 'navBar.logOut',
     onClick: 'logOut',
     icon: 'lock'
