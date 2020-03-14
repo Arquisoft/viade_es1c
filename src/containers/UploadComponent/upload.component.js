de7d25f15eeefcffdce91bac017275acbb074c37
UO266047 <UO266047@uniovi.es>
2020-03-14 03:01:10 +0100

    Version4 upload files to POD

--- src/containers/UploadComponent/upload.component.js
+++ src/containers/UploadComponent/upload.component.js
@@ -2,21 +2,30 @@ import React from "react";
 import { UploadWrapper, H1, MyInput,Button } from './upload.style';
 import { useTranslation } from "react-i18next";
 
-//const Uploader = require("../UploadPod/uploadToPod");
 import UploadToPod from '../UploadPod/uploadToPod';
+import {SolidFileClient} from "solid-file-client";
 
 const auth = require("solid-auth-client");
+ const file=require("solid-file-client");
 
 export const UploadComponent = props => {
   const { t } = useTranslation();
   const userWebId="https://raquel.solid.community/profile/card#me";
   const uploader = new UploadToPod(auth.fetch);
+
+	//const fileC=new SolidFileClient(file.fe);
+
   return(
      <UploadWrapper>
        <H1>{t('upload.title')}</H1>
        <MyInput type="file"/>
 	            
-	<Button className="ids-link-filled" onClick={uploader.executeSPARQLUpdateForUser(userWebId.replace("profile/card#me", "private/n.txt"),"Esto es una prueba de ASW")}>
+	<Button className="ids-link-filled" onClick={
+		SolidFileClient.createFile("https://raquel.solid.community/private/n2.txt",
+		"Esto es una prueba de ASW",
+		'text/plain', 'LINKS.INCLUDE')
+		
+	}>
 				{t("Añadir al pod")}
      </Button>
      </UploadWrapper>
