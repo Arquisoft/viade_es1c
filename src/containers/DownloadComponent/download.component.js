efdacc4007b545792cdb84a418cbfbb5da3f9adb
NourdineSatour <UO265379@uniovi.es>
2020-03-15 13:44:11 +0100

    Acces to the files in the pod

--- src/containers/DownloadComponent/download.component.js
+++ src/containers/DownloadComponent/download.component.js
@@ -1,10 +1,42 @@
 import React from "react";
 import { DownloadWrapper, H1, H3, Button, Input } from './download.style';
 import { useTranslation } from "react-i18next";
-import SolidAuth from 'solid-auth-client';
+import FC from 'solid-file-client';
+import auth from 'solid-auth-client';
 
 
+const fc = new FC(auth);
 
+
+export async function downloadRoutes(url){
+  var urlFolder = fc.readFolder(url);
+  var routes = new Array();
+  //console.log(routes);
+  urlFolder.then(function(result, routes){
+    //var i;
+    //for(i=0; i<result.files.length;i++){
+    //  console.log(result.files[i]);
+    //  console.log(result.files[i]);
+    //}
+      //console.log(result.files[1]);
+      console.log(result.files[1].url);
+      var urlR=fc.readFile(result.files[1].url);
+      urlR.then(function(txt){
+        console.log(txt);
+      });
+  }
+  );
+}
+
+function getUrl(){
+  var txt = document.getElementById("txtUrl");
+  return txt.value;
+}
+
+async function searchRoute() {
+  var url = getUrl();
+  downloadRoutes(url);
+}
 export const DownloadComponent = props => {
   const { t } = useTranslation();
   //const doc = SolidAuth.fetch('https://sandracast.solid.community/rutaDePrueba.json');
@@ -12,8 +44,10 @@ export const DownloadComponent = props => {
     <DownloadWrapper>
       <H1>{t('download.title')}</H1>
       <H3>{t('download.instruction')}</H3>
-      <Input type="text"></Input>
-      <Button className="ids-link-filled" > {t('download.button')}</Button>
+      <Input id = "txtUrl" type="text"></Input>
+      <Button className="ids-link-filled" onClick={searchRoute}> 
+      {t('download.button')}
+      </Button>
     </DownloadWrapper>
     
   );
