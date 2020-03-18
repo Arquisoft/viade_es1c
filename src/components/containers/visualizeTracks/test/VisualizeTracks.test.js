44170c88eadf7428c47b2f5af3764f0f788d0c28
Alberto <uo257180@uniovi.es>
2020-03-18 19:38:12 +0100

    fixing test 3

--- src/components/containers/visualizeTracks/test/VisualizeTracks.test.js
+++ src/components/containers/visualizeTracks/test/VisualizeTracks.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Map from '../VisualizeTrack';
+import Select from '../VisualizeTrack';
 
 describe('Visualize Map', () => {
   afterAll(cleanup);
 
   const { container } = render(
     <Router>
-      <Map />
+      <Select />
     </Router>
   );
 
