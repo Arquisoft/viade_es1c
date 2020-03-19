0dedeb901cd7625c1eaeb88722773550831d16e0
Miguel Ornia <UO250732@uniovi.es>
2020-03-19 18:54:47 +0100

    Starting with login

--- src/components/containers/login/Login.js
+++ src/components/containers/login/Login.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import {useTranslation} from "react-i18next";
-import {LoggedOut, LoggedIn, LoginButton} from '@solid/react';
+import {LoggedOut, LoggedIn } from '@solid/react';
+import { Button, FormGroup, FormControl } from "react-bootstrap";
 import {Redirect} from "react-router-dom";
 import "./Login.css";
 
 export const Login = props => {
     const {t} = useTranslation();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
     return (
-        <div className="login-div" id="login-container">
+        <div className="login-div">
             <LoggedOut>
-                <LoginButton popup="popup.html"/>
+                <div className="Login">
+                    <h2>{t("login.title")}</h2>
+                    <form onSubmit={handleSubmit}>
+                        <Button>
+                            {t("login.register")}
+                        </Button>
+                        <h3>{t("login.loginTitle")}</h3>
+                        <Button variant="light">
+                            {t("login.formButtonText")}
+                        </Button>
+                    </form>
+                </div>
             </LoggedOut>
             <LoggedIn>
                 <Redirect to="/welcome"></Redirect>
