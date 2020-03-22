59068ebad8697a020dace87708ba1b3d9a56507d
Miguel Ornia <UO250732@uniovi.es>
2020-03-22 01:06:34 +0100

    Fixing i18n

--- src/i18n.js
+++ src/i18n.js
@@ -3,10 +3,12 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+const backend = new Backend(null, { loadPath: "locales/{{lng}}/{{ns}}.json" });
+
 i18n
   // load translation using xhr -> see /public/locales
   // learn more: https://github.com/i18next/i18next-xhr-backend
-  .use(Backend)
+  .use(backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
@@ -14,14 +16,14 @@ i18n
   // Alternative use the I18nextProvider: https://react.i18next.com/components/i18nextprovider
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en-US',
+    fallbackLng: 'en',
     debug: false,
-    whitelist: ['en-US', 'en', 'es'],
+    whitelist: ['en', 'es'],
     // special options for react-i18next
     // learn more: https://react.i18next.com/components/i18next-instance
     react: {
       wait: true,
-        useSuspense: false
+      useSuspense: false
     }
   });
 
