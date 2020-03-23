2f431761dc07dc598f3fab5ec9de3a0aee42675d
NourdineSatour <UO265379@uniovi.es>
2020-03-23 18:51:23 +0100

    Now just enter the name of the route to download

--- src/components/containers/downloadTracks/DownloadTrack.js
+++ src/components/containers/downloadTracks/DownloadTrack.js
@@ -8,27 +8,33 @@ import auth from 'solid-auth-client';
 
 const fc = new FC(auth);
 
+var urlGlobal="";
 
-export async function getRoutes(url){
-    var folder = await fc.readFolder(url);
+function setGlobal(newUrl){
+    urlGlobal = newUrl;
+}
+
+export async function downloadRoute(){
+    console.log(urlGlobal);
+    //var folder = await fc.readFolder(url);
     //console.log(folder);
-    var files = new Array();
-    var i;
-    for(i=0;i<folder.files.length;i++)
-    {
-        //console.log(folder.files[i]);
-        files[i]=folder.files[i];
-    }
-    for(i=0;i<files.length;i++)
-    {
-        files[i]=folder.files[i];
-    }
-    console.log(files);
-    var file = await fc.readFile(files[1].url);
+    //var files = new Array();
+    //var i;
+    //for(i=0;i<folder.files.length;i++)
+    //{
+    //    //console.log(folder.files[i]);
+    //    files[i]=folder.files[i];
+    //}
+    //for(i=0;i<files.length;i++)
+    //{
+    //    files[i]=folder.files[i];
+    //}
+    //console.log(files);
+    var file = await fc.readFile(urlGlobal);
     console.log(file);
     var blob = new Blob([file], {type: 'application/json'});
     console.log(blob)
-
+//
     var d = document.getElementById("downFile");
     d.href= URL.createObjectURL(blob);
     d.download="prueba.json";
@@ -36,15 +42,21 @@ export async function getRoutes(url){
     console.log(d);
 }
 
-function getUrl(){
-  var txt = document.getElementById("txtUrl");
-  return txt.value;
-}
 
 async function searchRoute() {
-  var url = getUrl();
-  var routes = getRoutes(url);
-  //downloadRoutes(url,routes);
+    const auth = require('solid-auth-client');
+    auth.trackSession(session => {
+        if (!session) {
+            return;
+        } else {
+            var txt = document.getElementById("txtUrl").value;
+
+            let webId = session.webId;
+            let urlRoute = webId.slice(0, webId.length - 15).concat("public/MyRoutes/"+ txt +".json");
+            setGlobal(urlRoute);
+            downloadRoute();
+        }
+    }); 
 }
 
 export const DownloadTrack = props => {
@@ -62,7 +74,7 @@ export const DownloadTrack = props => {
                             <input id = "txtUrl" type="text"></input>
                         </div>
                         <div className="modal-footer">
-                            <a href="" id="downFile"></a>
+                            <a href="" id="downFile"> </a>
                             <Button onClick={searchRoute} > {t('download.button')}</Button>
                         </div>
                     </div>
