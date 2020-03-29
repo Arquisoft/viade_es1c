b967501ce121d3b78b95e2b4c268afcb14bc8623
NourdineSatour <UO265379@uniovi.es>
2020-03-29 18:55:06 +0200

    Found the Solid Community button in the popup

--- e2e/step-definitions/login.steps.js
+++ e2e/step-definitions/login.steps.js
@@ -9,6 +9,9 @@ defineFeature(feature, test => {
 	})
 	
   test('We want to login into Viade', ({ given, when, then, and }) => {
+
+    let popup;
+
     given('The login page', async() => {
       
       await expect(page).toMatchElement('h2', { text: 'Welcome to ViaDe_ES1C' })
@@ -26,8 +29,13 @@ defineFeature(feature, test => {
     then('I expect a new window to appear', async () => {
       await expect(popup.title()).resolves.toMatch('Select your Identity Provider');
       
-      await expect(popup).toMatchElement('button', { text: "Go" });
-      await expect(popup).toClick('form > button', { text: "Go" });
+      await expect(popup).toMatch("Solid Community", { timeout: 1000 });
+
+      //await expect(popup).toMatchElement('button', { text: "Go" }, { timeout: 6000 });
+
+      //await expect(popup).toClick("div > div > div > button",{ text: "Solid Community" });
+      
+      //await expect(popup).toMatch("Login", { timeout: 5000 });
       //Hasta aqui bien 
 
       
