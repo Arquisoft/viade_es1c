a279b55166cd3ae8106dfe25115befc219f3b5ce
UO263544 <UO263544@uniovi.es>
2020-03-31 17:46:56 +0200

    init test downloadTrack

---
+++ src/tests/downloadTrack.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import { Button } from 'react-bootstrap';
+import DownloadService from '../services/DownloadService';
+
+const r1='../tracks/rutaDePrueba3'
+
+var upl=new DownloadService();
+
+test('Test download', () => {
+  
+}); 
