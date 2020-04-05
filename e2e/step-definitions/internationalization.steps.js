57a1f27e827b22ff1f3a2f1ef8eecfe660e36ce4
NourdineSatour <UO265379@uniovi.es>
2020-04-05 18:20:46 +0200

    Test

--- e2e/step-definitions/internationalization.steps.js
+++ e2e/step-definitions/internationalization.steps.js
@@ -16,11 +16,8 @@ defineFeature(feature, test => {
 
         when('The user press the boton Language or Idioma (depending on the language you have selected) and select the other language', async () => {
             await expect(page).toMatchElement('a', { href: 'https://arquisoft.github.io/viade_es1c/docs/'});
-            await expect(page).toClick('a', { href: 'https://arquisoft.github.io/viade_es1c/docs/'});
-            
-
-            
-
+            await expect(page).toClick('Button', { classname: 'logout btn btn-light'});
+            await expect(page).toMatchElement('a', { href: 'https://solid.inrupt.com/get-a-solid-pod'});
         });
   
         then('All words on the page change will change language to the one selected by the user', async () => {});
