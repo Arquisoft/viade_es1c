6ba55d024d543268ccaf64e7aca5e16705966c55
NourdineSatour <UO265379@uniovi.es>
2020-04-05 19:36:22 +0200

    Try realize unit tests

---
+++ src/tests/uploadTracks.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { render, fireEvent } from "@testing-library/react";
+
+import UploadTrack from "./../components/containers/uploadTracks/UploadTrack.js";
+
+test('Prueba', async () => {
+
+  expect(2+2).toBe(4);
+  
+  //const correctValues = { email: 'test@example.org', remail: 'test@example.org' };
+
+  //const { getByLabelText, getByText, container } = render(<EmailForm/>);
+  //
+  //const inputEmail = getByLabelText('email-input');
+  //const inputRemail = getByLabelText('remail-input');
+
+  //fireEvent.change(inputEmail, { target: { value: correctValues.email }});
+  //expect(getByText(/Submit/i).closest('button')).toHaveAttribute('disabled');
+  //
+  //fireEvent.change(inputRemail, { target: { value: correctValues.remail }});
+  //expect(getByText(/Submit/i).closest('button')).not.toHaveAttribute('disabled');
+  
+});
