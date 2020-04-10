f2cb548e88c6e7493ffba621024643bdb21401c8
NourdineSatour <UO265379@uniovi.es>
2020-04-10 18:58:42 +0200

    Improvement in the all test

--- src/tests/welcome.test.js
+++ src/tests/welcome.test.js
@@ -5,6 +5,7 @@ import { render} from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 
+
 it("renders without crashing",() => {
     const div = document.createElement("div");
     ReactDOM.render(<Welcome></Welcome>, div);
