925b3f06d97b855852d4dd1cd7f8543604f976da
UO266047 <UO266047@uniovi.es>
2020-04-10 19:41:26 +0200

    Increasing implementation of test and solvinf codacy problems

--- src/tests/visualizeTrack.test.js
+++ src/tests/visualizeTrack.test.js
@@ -4,12 +4,22 @@ import VisualizeTrack from "../components/containers/visualizeTracks/VisualizeTr
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-it("renders without crashing",() => {
+it("VisualizeTrack -> renders without crashing",() => {
     const div = document.createElement("div");
     ReactDOM.render(<VisualizeTrack></VisualizeTrack>, div);
 })
 
-it("renders button correctly", () => {
+it("VisualizeTrack -> renders visualize correctly", () => {
     const {getByTestId} =render(<VisualizeTrack></VisualizeTrack>);
     expect(getByTestId("visualizeTest"));
+});
+
+it("VisualizeTrack -> component visualizeTrack has the correct subComponents", () => {
+    expect(document.getElementsByClassName("myH1")).toBeTruthy();
+    expect(document.getElementsByClassName("map")).toBeTruthy();
+    expect(document.getElementsByClassName("h4-format")).toBeTruthy();
+    expect(document.getElementsByClassName("formal-div")).toBeTruthy();
+    expect(document.getElementsByClassName("button-margin")).toBeTruthy();
+    expect(document.getElementsByClassName("visualizeButton")).toBeTruthy();
+    expect(document.getElementsByClassName("select-format")).toBeTruthy();
 });
