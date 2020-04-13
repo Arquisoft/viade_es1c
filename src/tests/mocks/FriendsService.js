1e186a581c36767637e00dca105b7ea1e0f7abd3
Miguel Ornia <UO250732@uniovi.es>
2020-04-13 22:47:49 +0200

    Improving friend test

--- src/tests/mocks/FriendsService.js
+++ src/tests/mocks/FriendsService.js
@@ -1,18 +1,36 @@
 export default class FriendsService {
 
+  constructor() {
+    this.friends = [];
+  }
+
   check(friendWebId) {
-    return true;
+    for (let i = 0; i < this.friends.length; i++) {
+      if (this.friends[i].localeCompare(friendWebId) === 0) {
+        return true;
+      }
+    }
+    return false;
   }
 
   exists(friendWebId) {
-    return true;
+    for (let i = 0; i < this.friends.length; i++) {
+      if (this.friends[i].localeCompare(friendWebId) === 0) {
+        return true;
+      }
+    }
+    return false;
   }
 
   add(friendWebId) {
-
+    this.friends.push(friendWebId);
   }
 
   delete(friendWebId) {
-
+    for (let i = 0; i < this.friends.length; i++) {
+      if (this.friends[i].localeCompare(friendWebId) === 0) {
+        this.friends.splice(i);
+      }
+    }
   }
 }
