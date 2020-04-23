9a6147e554456718c139cb3469e895c3e86495e2
UO266047 <UO266047@uniovi.es>
2020-04-23 19:50:16 +0200

    Testing multimedia

--- src/tests/mocks/VisualizeService.js
+++ src/tests/mocks/VisualizeService.js
@@ -7,12 +7,13 @@ export default class VisualizeService {
     this.points = [];
     this.elevationsValues = [];
     this.existsMultimedia = true;
-    this.images = [];
-    this.videos = [];
+    this.images = ["https://miguelornia.solid.community/viade/resources/83357_2880_1800.jpg","https://miguelornia.solid.community/viade/resources/logo192.png"];
+    this.videos = ["https://miguelornia.solid.community/viade/resources/video.mp4","https://miguelornia.solid.community/viade/resources/video1.mp4"];
     this.permissionsImage = true;
     this.permissionsVideo = true;
     this.existsVideo = true;
     this.existsImage = true;
+	
 	this.mostrar=false;
   }
   
